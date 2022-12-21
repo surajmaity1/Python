e95509e87ec58e63bdba96005e024b3061d1b2cb
surajmaity1 <surajmaity981@gmail.com>
2022-12-21 19:44:19 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from math import *
+# use of " inside a string
+print("Hey \" Suraj")
+
+name = "Suraj Maity"
+print(name + " is cool")
+
+print(name.upper())
+print(name.isupper())
+
+print(name[0])
+print(name.index('a'))
+
+# find starting index of a string
+print(name.index("Mai"))
+
+# replace
+print(name.replace("Suraj", "Sudipta"))
+
+
+# number operation
+my_num = 3.4
+print(round(my_num))
+print(abs(my_num))
+print(floor(my_num))
+print(ceil(my_num))
---
+++ list.py
@@ -0,0 +1,11 @@
+friendList = ["Suraj", "Aniket", "Rakesh", "Rohit", "Darshan"]
+
+print(friendList[1:])   # it will print from index 1 to last
+print(friendList[1:4])  # it will print up to index 3 not including 4
+
+# append list
+newList = ["Chandra", "Sandip"]
+friendList.extend(newList)
+
+print(friendList)
+
